29d439eb20cf9d4e8b71b3721edc343ce6d94fff
Jenni Rinker <jennifer.rinker@duke.edu>
2015-07-21 15:35:49 -0600

    Re-ran demo to produce 500 random processed, saved md.py

--- misc/demo-Obukhov_length_random_records.py
+++ misc/demo-Obukhov_length_random_records.py
@@ -23,45 +23,45 @@ dir2   = 315                        # CW edge for direction range
 preLim = 2.7                        # lower precipitation limit
 
 # column indices for each value
-CScol  = fields.index('Wind_Speed_Cup')
-dirCol = fields.index('Wind_Direction')
-preCol = fields.index('Precipitation')
-
-i_rec = 0
-metadata = np.empty((n_recs,len(fields)))
-while i_rec < n_recs:
-    
-    # draw random time stamp/height
-    year   = random.randint(yearRng[0],yearRng[1])
-    month  = random.randint(1,12)
-    day    = random.randint(1,31)
-    hour   = random.randint(0,23)
-    minute = random.randint(0,6)*10
-    height = random.choice([15,30,50,76,100,131])
-    time_tup = (year,month,day,hour,minute)
-        
-    # check if it exists
-    fpath = jr.NRELtime2fpath(time_tup)
-    if len(fpath) > 0:
-        
-        # load structure
-        struc = scio.loadmat(fpath)
-        
-        # calculate parameters
-        row = jr.extractNRELparameters(struc,height)
-        
-        flagCS   = row[CScol] > CSLim
-        flagDir1 = row[dirCol] >= dir1
-        flagDir2 = row[dirCol] <= dir2
-        flagPrec = row[preCol] >= preLim
-        flagNaN  = not np.isnan(row[6])
-        
-        if (flagCS and flagDir1 and flagDir2 and flagPrec and flagNaN):
-            metadata[i_rec] = row.reshape(1,len(fields))
-    
-            i_rec += 1
-                                    
-            if not (i_rec % 5): print(i_rec)
+#CScol  = fields.index('Wind_Speed_Cup')
+#dirCol = fields.index('Wind_Direction')
+#preCol = fields.index('Precipitation')
+#
+#i_rec = 0
+#metadata = np.empty((n_recs,len(fields)))
+#while i_rec < n_recs:
+#    
+#    # draw random time stamp/height
+#    year   = random.randint(yearRng[0],yearRng[1])
+#    month  = random.randint(1,12)
+#    day    = random.randint(1,31)
+#    hour   = random.randint(0,23)
+#    minute = random.randint(0,6)*10
+#    height = random.choice([15,30,50,76,100,131])
+#    time_tup = (year,month,day,hour,minute)
+#        
+#    # check if it exists
+#    fpath = jr.NRELtime2fpath(time_tup)
+#    if len(fpath) > 0:
+#        
+#        # load structure
+#        struc = scio.loadmat(fpath)
+#        
+#        # calculate parameters
+#        row = jr.extractNRELparameters(struc,height)
+#        
+#        flagCS   = row[CScol] > CSLim
+#        flagDir1 = row[dirCol] >= dir1
+#        flagDir2 = row[dirCol] <= dir2
+#        flagPrec = row[preCol] >= preLim
+#        flagNaN  = not np.isnan(row[6])
+#        
+#        if (flagCS and flagDir1 and flagDir2 and flagPrec and flagNaN):
+#            metadata[i_rec] = row.reshape(1,len(fields))
+#    
+#            i_rec += 1
+#                                    
+#            if not (i_rec % 5): print(i_rec)
                 
 # %% ============================== analysis ==================================
 
