7900876b4d1edcbac9c4b4fe43efdd5565a6bea4
Jenni Rinker <jennifer.rinker@duke.edu>
2015-08-06 17:22:11 -0600

    Began script to construct field from data

--- misc/tmp.py
+++ misc/tmp.py
@@ -1,24 +1,33 @@
 
-#import sys
-#libpath = 'C:\\Users\\jrinker\\Documents\\GitHub\\dissertation'
-#if (libpath not in sys.path): sys.path.append(libpath)
-#
-#import JR_Library.main as jr
-#
-#basedir = jr.getBasedir('NREL')
-#
-#list_mats = jr.list_matfiles(basedir,save=1)
-
+"""
+Testing expansion of time series points to new grid
+"""
 import numpy as np
-from joblib import Parallel, delayed
-import sys
 
-def fcn(i,n):
-    return np.array([np.nan,i,n*i])
-    
-if (__name__ == '__main__'):
-    N = 5
-    n = 1
-#    out = [fcn(i,n) for i in range(N)]
-    out = Parallel(n_jobs=2,verbose=1)(delayed(fcn)(i,n) for i in range(N))
-    print(out)
+y_data = np.array([0,0,0,0,0,0.])
+z_data = np.array([15,30,50,76.,100,131.])
+
+y_grid = np.linspace(-40,40,5)
+z_grid = np.linspace(20,160,7)
+n_y    = y_grid.size
+n_z    = z_grid.size
+
+n_data  = y_data.size
+n_grid  = n_y*n_z
+
+Y_grid, Z_grid = np.meshgrid(y_grid,z_grid)
+
+y_all = np.concatenate((y_data,Y_grid.reshape(n_grid)))
+z_all = np.concatenate((z_data,Z_grid.reshape(n_grid)))
+
+plt.scatter(y_all,z_all)
+
+#coh = 0.5
+#C = np.array([[1.,0.],[coh,np.sqrt(1-coh**2)]])
+#
+#X1u = 1.1*np.exp(1j*np.pi/4)
+#
+#U1u = X1u
+#U2u = np.abs(U1u) * np.exp(1j*np.random.rand()*2*np.pi)
+#
+#X2u = np.dot(C[1,:],np.array([[U1u],[U2u]]))
