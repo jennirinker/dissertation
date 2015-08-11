7626722c59d58d5882b147ec1aade484bf9a9c90
Jenni Rinker <jennifer.rinker@duke.edu>
2015-08-11 08:09:04 -0600

    Created separate files for updating M4, fluela, removed semicolons
    This ain't Matlab, get rid of those stupid semicolons.

---
+++ download-data/update_fluela.py
@@ -0,0 +1,24 @@
+
+if (__name__ == '__main__'):
+    """ Do when called as script from command line.    
+    """
+    from URLDataDownload import updateDirectory
+
+    # 20-Hz base URL and directory
+    #   *** NO TRAILING SLASHES ***
+    baseURL = 'http://wind.nrel.gov/MetData/135mData/' + \
+                'FluelaTwr/20Hz/mat'
+    basedir = 'G:\\data\\fluela-high_freq'
+
+    # update the directory, return list of err'd files
+    errList = updateDirectory(baseURL,basedir)
+
+    # print a comment if any errors
+    if errList:
+        print('\n{} file(s) '.format(len(errList)) \
+            + 'with errors saved in errList.')
+
+    # and that's it!
+    print '\nScript complete.\n'
+
+    
