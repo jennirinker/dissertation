774c0ad8c9354484b9a2e317b6c406fdf1d027fb
Jenni Rinker <jennifer.rinker@duke.edu>
2015-08-18 14:29:00 -0600

    Copied file from hard drive
    Changes include commenting out the library path (diff on was13),
    changing dataset flag, and changing where we define the out
    filename

--- metadata-process/proc-clifton_metadata.py
+++ metadata-process/proc-clifton_metadata.py
@@ -9,17 +9,16 @@ import numpy as np
 import scipy.io as scio
 
 if (__name__ == '__main__'):
-    libpath = 'C:\\Users\\jrinker\\Documents\\GitHub\\dissertation'
+#    libpath = 'C:\\Users\\jrinker\\Documents\\GitHub\\dissertation'
 #    libpath = 'E:\\NREL-metadata-process'
-    if (libpath not in sys.path): sys.path.append(libpath)
+#    if (libpath not in sys.path): sys.path.append(libpath)
     import JR_Library.main as jr
     
     # variables for later
-    dataset  = 'NREL'
+    dataset  = 'fluela'
     basedir  = jr.getBasedir(dataset)
-    njobs    = 4
+    njobs    = 60
     fields   = jr.metadataFields(dataset)
-    foutname = dataset + '-metadata.mat'
     
     # load saved list of mat files
     lmats_fname = [fp for fp in os.listdir(basedir) if 'listmats' in fp][0]
@@ -29,11 +28,13 @@ if (__name__ == '__main__'):
     n_files = len(list_mats)
         
     # process files in parallel
-#    md_list = Parallel(n_jobs=njobs,verbose=9) \
-#        (delayed(jr.NRELlistmetadata)(i,list_mats) for i in range(n_files))
-    n_proc = 1
+    n_proc = n_files                            # no. files to process
     md_list = Parallel(n_jobs=njobs,verbose=9) \
         (delayed(jr.listmetadata)(dataset,i,list_mats) for i in range(n_proc))
+#    md_list = []
+#    for i in range(n_proc):
+#        tmp = jr.listmetadata(dataset,i,list_mats)
+#        md_list.append(tmp)
         
     # convert list of arrays to metadata
     n_heights = jr.datasetSpecs(dataset)[2].size
@@ -42,7 +43,8 @@ if (__name__ == '__main__'):
         metadata[n_heights*i:n_heights*(i+1)] = np.asarray(md_list[i])
         
     # save output
-#    mdict             = {}
-#    mdict['fields']   = fields
-#    mdict['metadata'] = metadata
-#    scio.savemat(foutname,mdict)
+    foutname = dataset + '-metadata.mat'
+    mdict             = {}
+    mdict['fields']   = fields
+    mdict['metadata'] = metadata
+    scio.savemat(foutname,mdict)
