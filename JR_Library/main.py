f437cd560e6af6f75da4d98e4ff7ca5f470b1eaa
Jenni Rinker <jennifer.rinker@duke.edu>
2015-10-10 11:05:48 -0600

    Created fcn to load numeric data from txt file with header/units

--- JR_Library/main.py
+++ JR_Library/main.py
@@ -405,6 +405,53 @@ def readInput_v2(filename):
         print 'That is neither a .sum or .inp';
         return [];
 
+
+def mygenfromtxt(fname,header=True,units=False,delimiter='\t'):
+    """ Load delimited array into numpy with optional header and units. Assumes
+        units line will only be present if header line present.
+    
+        Args:
+            fname (string): filename or path to file
+            header (Boolean): whether to read header line
+            units (Boolean): whether to read units line
+            delimiter (string): string used to separate values
+            
+        Returns:
+            data (numpy array): numeric data read from the text file
+            header (list): strings from header line (if header=True)
+            units (list): strings from units line (if header,units=True)
+    """
+    import numpy as np
+    
+    # read header and unit lines if appropriate
+    with open(fname,'r') as f:
+        
+        # read header line if present
+        if header:
+            header_text = f.readline().strip('\n').split('\t')
+            
+            # read units line if present
+            if units:
+                units_text = f.readline().strip('\n').split('\t')
+            else:
+                units_text = []
+                
+        else:
+            header_text = []
+            
+    # use numpy to load numeric data
+    data = np.genfromtxt(fname,delimiter=delimiter,
+                         skip_header=header+units)
+    
+    # return different values based on inputs
+    if not header:
+        return data
+    elif (header and (not units)):
+        return data, header_text
+    else:
+        return data, header_text, units_text
+
+
 # %%============================================================================
 # METADATA PROCESSING
 # ==============================================================================
