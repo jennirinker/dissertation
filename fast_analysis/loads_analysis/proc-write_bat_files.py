38d1bcb793ddc6b8cf51088ca59564aca4ef259b
Jenni Rinker <jennifer.rinker@duke.edu>
2015-12-20 18:58:02 -0700

    Deleted unused files

--- fast_analysis/loads_analysis/proc-write_bat_files.py
+++ fast_analysis/loads_analysis/proc-write_bat_files.py
@@ -5,7 +5,6 @@ import sys
 libpath = 'C:\\Users\\jrinker\\Documents\\GitHub\\public\\nwtc_python_tools'
 if (libpath not in sys.path): sys.path.append(libpath)
 
-import jr_fast
 import os
 import numpy as np
 import datetime
